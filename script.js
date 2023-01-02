d73a8e40a493886b2174cc2e335a7bc2deb978b0
FredrikEkmark <fredrik.ekmark@gmail.com>
2023-01-02 22:23:01 +0100

    Javascript message sent fixed

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const sendMessageEl = document.getElementById("sendMessage");
 
 const formEl = document.getElementById("form");
 
-function messageSent() {
-    formEl.innerHTML = "test";
-};
-
-sendMessageEl.addEventListener('click', messageSent());
-;
+sendMessageEl.addEventListener('click', function messageSent() {
+    formEl.innerHTML = "Message sent"
+});
